bd213ea27608425edeeb9bc6c1727e50145b57e3
Agosta Salvatore <agosta.boolean@gmail.com>
2026-03-04 00:44:14 +0100

    first commit

---
+++ resources/js/Pages/AiGenerator.jsx
@@ -0,0 +1,8 @@
+export default function AiGenerator() {
+    return (
+        <div>
+            <h1>AI Content Generator</h1>
+            <p>Qui genereremo articoli, post LinkedIn e hashtag.</p>
+        </div>
+    );
+}
